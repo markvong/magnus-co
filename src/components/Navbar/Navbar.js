46e46d3327465d932058ecbcb92e2a1335648873
markvong <mark.m.vong@gmail.com>
2020-12-16 02:18:25 -0800

    Last login func restored and relocated login/logout button to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,12 +4,64 @@ import { useOktaAuth } from "@okta/okta-react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const curr_user_endpoint = `https://dev-8181045.okta.com/api/v1/users/me`;
+
   const { oktaAuth, authState } = useOktaAuth();
   const history = useHistory();
+  const [currLogin, setCurrLogin] = useState(null);
+
   const login = async () => history.push("/login");
   const logout = async () => {
-    oktaAuth.signOut();
+    // oktaAuth.signOut();
+    setLastLoginAndSignOut();
   };
+
+  const getLastLogin = async () => {
+    if (authState.isAuthenticated) {
+      const accessToken = authState.accessToken["value"];
+      const options = {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      };
+
+      fetch(curr_user_endpoint, options)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setCurrLogin(data["lastLogin"]);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const setLastLoginAndSignOut = async () => {
+    if (currLogin && authState.isAuthenticated) {
+      const accessToken = authState.accessToken["value"];
+      const method = "POST";
+      const headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`
+      };
+
+      const body = JSON.stringify({ profile: { last_Login: currLogin } });
+
+      const options = {
+        method,
+        headers,
+        body
+      };
+
+      fetch(curr_user_endpoint, options)
+        .then((response) => response.text())
+        .then((res) => {
+          oktaAuth.signOut();
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
   const loginBtn = (
     <button onClick={login} className="nav-log-btn btn btn-primary">
       Login
@@ -28,6 +80,10 @@ const Navbar = () => {
   ) : (
     logoutBtn
   );
+
+  useEffect(() => {
+    getLastLogin();
+  }, [authState]);
 
   return (
     <div id="nav-bar">
